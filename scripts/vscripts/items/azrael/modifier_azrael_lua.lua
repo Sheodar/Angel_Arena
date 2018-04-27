c6d39913dac22a74798bd26670df2fef49003b27
Sheodar <kkabachok@mail.ru>
2018-04-28 00:55:52 +0300

    azrael 2.0

--- scripts/vscripts/items/azrael/modifier_azrael_lua.lua
+++ scripts/vscripts/items/azrael/modifier_azrael_lua.lua
@@ -8,11 +8,13 @@ end
 --------------------------------------------------------------------------------
 function modifier_azrael_lua:OnCreated(kv)
     self.cooldown = self:GetAbility():GetSpecialValueFor("cooldown")
+    self.number_of_attacks = self:GetAbility():GetSpecialValueFor("number_of_attacks")
 end
 
 --------------------------------------------------------------------------------
 function modifier_azrael_lua:OnRefresh(kv)
     self.cooldown = self:GetAbility():GetSpecialValueFor("cooldown")
+    self.number_of_attacks = self:GetAbility():GetSpecialValueFor("number_of_attacks")
 end
 
 -------------------------------------------------------------------------------
@@ -27,7 +29,6 @@ end
 function modifier_azrael_lua:OnAttack(params)
     if not IsServer() then return end
     local caster = params.attacker
-
     if caster:FindModifierByName("azrael_lua_stopspam_lua") then return end
     if caster:FindModifierByName("azrael_lua_cooldown_lua") then return end
 
@@ -37,13 +38,32 @@ function modifier_azrael_lua:OnAttack(params)
     if caster:GetUnitName() == "npc_dota_hero_weaver" or not caster:IsRangedAttacker() or caster:IsIllusion() or caster:IsHexed() then return end
 
     local attack_time = 1 / caster:GetAttacksPerSecond()
-    local delay = attack_time / 3
-    caster:AddNewModifier(caster, self:GetAbility(), "azrael_lua_stopspam_lua", { duration = delay })
+    local delay = attack_time / 2 / self.number_of_attacks
 
-    Timers:CreateTimer(delay / 1.5,
+    caster:AddNewModifier(caster, self:GetAbility(), "azrael_lua_stopspam_lua", { duration = attack_time-0.05 })
+
+    Timers:CreateTimer(delay,
         function()
             caster:PerformAttack(target, true, true, true, true, true, false, false)
+            if self.number_of_attacks > 1 then
+                self.number_of_attacks = self.number_of_attacks -1
+                for i = 1, self.number_of_attacks do
+                    Timers:CreateTimer(delay / self.number_of_attacks * i,
+                        function()
+                            caster:PerformAttack(target, true, true, true, true, true, false, false)
+                        end)
+                end
+            end
         end)
+
+
+    --local delay = attack_time / 3
+    --caster:AddNewModifier(caster, self:GetAbility(), "azrael_lua_stopspam_lua", { duration = delay })
+
+    --Timers:CreateTimer(delay / 1.5,
+    --  function()
+    --    caster:PerformAttack(target, true, true, true, true, true, false, false)
+    --end)
 end
 
 -------------------------------------------------------------------------------
